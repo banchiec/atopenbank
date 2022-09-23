f3dcaaf8e17843cc2fe3ae8de10b66283ad185b1
banchiec <icaro_19@hotmail.com>
2022-09-23 10:26:20 +0200

    Add data.js

---
+++ src/utils/data.js
@@ -0,0 +1,28 @@
+import Group from '../assets/img/group.svg'
+import Group3 from '../assets/img/group-3.svg'
+
+export const dataInfo = [
+	{
+		id: 1,
+		img: Group,
+		text: 'Guarda aquí todas tus contraseñas, datos o cualquier información, olvida las notas de papel y las aplicaciones no protegidas.',
+	},
+	{
+		id: 2,
+		img: Group3,
+		text: 'Crea tu clave maestra: solo tú podras acceder a tus secretos con ella.',
+	},
+]
+
+export const textInfo = [
+	{
+		id: 1,
+		title: 'Cómo funciona',
+		text: 'En primer lugar, debes crear una contraseña diferente para sus pertenecias electrónicas, No podrás recuperar tu contraseña, así que recuérdala bien.',
+	},
+	{
+		id: 2,
+		title: 'Qué datos puedes guardar',
+		text: 'Por ejemplo, el número de tu tarjeta, el PIN y el PUK de tu teléfono móvil, el número de serie de alguno de tus dispositivos o cualquier información que necesites tener en un lugar seguro.',
+	},
+]
